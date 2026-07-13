8d25d5d4560804e41c582ad6fbd52c93fde014f0
Eashanthsai <eashanthsaineella@gmail.com>
2026-07-13 23:06:34 +0530

    Day 2 of Python Series

--- Day2.py
+++ Day2.py
@@ -4,3 +4,16 @@ print('''
       This is used to write multiple lines like a sentence.
         This is also used to write a whole paragraph in python.  
       ''')
+# and I am going to learn about the variables and then performing some operations on them.
+x = 3
+y = 4
+sum = x+y
+print(sum)  # This line is to print the sum of two numbers
+name = "Eashanth sai" # This line is to declare a string variable
+is_Student = True # This line is to declare a Boolean variable
+num = str(3)# This line is to convert the integer into string
+num1 = int(3)   # This line is to convert the string into integer
+print(num)  # This line is to print the string variable
+print(num1)  # This line is to print the integer variable
+print(name)  # This line is to print the string variable
+print(is_Student)  # This line is to print the Boolean variable
