1d99967b8efe7b6be33c8c5c5951a7dd0c2ae118
Eashanthsai <eashanthsaineella@gmail.com>
2026-07-13 22:59:47 +0530

    Day two of Python series

---
+++ Day2.py
@@ -0,0 +1,6 @@
+# Today I am going to learn about the commenting and I guess this is called Commenting in python
+print(123456)  # This line is to learn how to print the numbers in python
+print('''
+      This is used to write multiple lines like a sentence.
+        This is also used to write a whole paragraph in python.  
+      ''')
