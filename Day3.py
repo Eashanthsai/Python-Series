c801662cc63e085b3a03671e027ffbb45a220ee8
Eashanthsai <eashanthsaineella@gmail.com>
2026-07-14 23:14:32 +0530

    Day-3 of python series

---
+++ Day3.py
@@ -0,0 +1,11 @@
+#Today I am going to learn about the Type casting in python and I guess this is called Type Casting in python
+x = int(3)
+y = int(4.2)
+y1 = 4.1
+print(x)
+print(y)# in this line I actually converted  the float (decimal) into integer and printed it
+print(type(x))
+print(type(y1))
+name = "Eashanth sai"
+print(type(name))
+print(name)
